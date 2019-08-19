f4068225ef2bdf1ec375884ed3383f8ef5738443
DELL <DELL@DESKTOP-9TMQGJQ>
2019-08-19 23:55:29 +0200

    This is Java Swing GUI Kilogram Converter Project

---
+++ src/kilogramconverter/KiloGramConverter.java
@@ -0,0 +1,17 @@
+package kilogramconverter;
+
+/**
+ *
+ * @author Nouran
+ */
+public class KiloGramConverter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        KiloConv KC = new KiloConv();
+        // TODO code application logic here
+    }
+    
+}
